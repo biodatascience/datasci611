c876b54818374b82d9f24ddf9a49883dd4a4da2d
Matt Biggs <mb3ad@virginia.edu>
2018-09-25 23:42:54 -0400

    updated makefile example

--- scripts/shakespeare/count_b4_thee.sh
+++ scripts/shakespeare/count_b4_thee.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 # Find the top ten words that occur before the word "thee"
 
-grep -Eio "\w+ thee" shakespeare_cw.txt | \
+grep -Eio "\w+ thee" $1 | \
 	cut -d ' ' -f 1 | \
 	sort | \
 	uniq -c | \
