93b923806f914a88db812a6989cde31f6a1ceff9
mbi2gs <mb3ad@virginia.edu>
2018-09-26 08:57:18 -0400

    Updated shakespeare example

--- scripts/shakespeare/dogberry.sh
+++ scripts/shakespeare/dogberry.sh
@@ -1,6 +1,6 @@
 # Extract all the quotes by the character DOGBERRY
 
-grep -Ei -A 10 "DOGBERRY" shakespeare_cw.txt |
+grep -Ei -A 10 "[A-Z]{3,}" $1 |
 	tr '\n\r' ' ' | \
-	sed 's/DOGBERRY/\nDOGBERRY/g' > dogberry_partial.txt
-	
+	sed -E 's/([A-Z]{3,})/\n\1/g'  |
+	grep -Ei "^DOGBERRY" > dogberry.txt
