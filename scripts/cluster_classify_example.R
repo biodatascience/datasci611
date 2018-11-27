bdde23677451690085bb5e24c775b6ee3092ae90
Matt Biggs <mb3ad@virginia.edu>
2018-11-26 22:22:36 -0500

    cluster and classification

--- scripts/cluster_classify_example.R
+++ scripts/cluster_classify_example.R
@@ -7,9 +7,11 @@ set.seed(0)
 
 # Generate example data set
 N = 30
-gene_df = rbind(tibble(gene_a = rnorm(n=N, mean=4.5), gene_b = rnorm(n=N, mean=1), explant_id = seq(1, N), type = 1),
-                tibble(gene_a = rnorm(n=N, mean=1), gene_b = rnorm(n=N, mean=1), explant_id = seq(N+1, 2*N), type = 2),
-                tibble(gene_a = rnorm(n=N, mean=1), gene_b = rnorm(n=N, mean=4.5), explant_id = seq((2*N)+1, 3*N), type = 3))
+U1 = 1
+U2 = 4.5
+gene_df = rbind(tibble(gene_a = rnorm(n=N, mean=U2), gene_b = rnorm(n=N, mean=U1), explant_id = seq(1, N), type = 1),
+                tibble(gene_a = rnorm(n=N, mean=U1), gene_b = rnorm(n=N, mean=U1), explant_id = seq(N+1, 2*N), type = 2),
+                tibble(gene_a = rnorm(n=N, mean=U1), gene_b = rnorm(n=N, mean=U2), explant_id = seq((2*N)+1, 3*N), type = 3))
 gene_df$type = as.factor(gene_df$type)
 
 ggplot(gene_df, aes(gene_a, gene_b)) +
@@ -30,6 +32,24 @@ ggplot(gene_df, aes(gene_a, gene_b, group=cluster)) +
   geom_point(size=2, alpha=0.75, aes(color=cluster))
 
 # How many clusters should we pass to the k-means algorithm?
+
+#-------------------------
+# Determine an appropriate 
+# number of clusters for the
+# data
+#-------------------------
+kmeans_wss <- Vectorize(function(c){
+  return(sum(kmeans(gene_df, centers=c)$withinss))
+})
+
+cluster_fit = tibble(num_centers=1:15) %>%
+  mutate(wss = kmeans_wss(num_centers), diff_wss = wss - lag(wss))
+
+ggplot(cluster_fit, aes(num_centers, wss)) +
+  geom_point() +
+  geom_line() +
+  labs(x="Number of Clusters",
+       y="Within groups sum of squares") 
 
 #-------------------------
 # Hierarchical clustering
@@ -66,7 +86,7 @@ new_samples = tibble(gene_a = rnorm(n=10, mean=1), gene_b = rnorm(n=10, mean=1))
 knn_fit = train(type ~ gene_a + gene_b, data = gene_df, method = "knn")
 
 # Predict type for new data
-knn_classification = predict(knn_fit, new_samples, type="class")
+knn_classification = predict(knn_fit, new_samples, type="raw")
 
 # Train a support vector machine
 svm_fit = train(type ~ gene_a + gene_b, data = gene_df, method = "svmLinear")
