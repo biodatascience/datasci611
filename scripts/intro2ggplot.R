18c0b141e59e93847fff2c65f0a6100beb2465e9
Matt Biggs <mb3ad@virginia.edu>
2019-03-21 21:36:23 -0400

    intro2ggplot script

---
+++ scripts/intro2ggplot.R
@@ -0,0 +1,78 @@
+library(ggplot2)
+
+# Preview the data
+diamonds
+
+# To start, assign variables to the x and y axes, 
+# and pick a geom (here I chose scatter plot or "geom_point")
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point()
+
+# Examples of other geoms
+ggplot(diamonds, aes(x=carat, y=price)) +
+  geom_hex()
+
+ggplot(diamonds, aes(x=carat, y=price)) +
+  geom_bin2d()
+
+# It's easy to add layers ... just tack on another "geom"
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point() +
+  geom_rug()
+
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point() +
+  geom_smooth()
+
+# To modify the appearance of the elements of a layer, 
+# add specifications for size, alpha (transparency), etc.
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point(size=4, color='red') +
+  geom_smooth()
+
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point(size=4, alpha=0.2) +
+  geom_smooth(size=3)
+
+# To link the appearance of the elements of a layer to 
+# variables in the data, use the same specifications as above
+# but define the dependency in an "aes" section
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point(size=4, aes(shape=cut)) +
+  geom_smooth() +
+  theme_bw()
+
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point(size=4, aes(color=cut, shape=color)) +
+  geom_smooth()
+
+# Facets are another way to map variables onto your visualization
+ggplot(diamonds,aes(x=carat, y=price)) +
+  geom_point(size=4) +
+  facet_wrap(~cut)
+
+# Statistical transformations incorporate summaries of your data 
+# easily into your visualization
+ggplot(diamonds,aes(x=price)) +
+  geom_histogram()
+
+ggplot(diamonds,aes(x=cut, y=price)) +
+  geom_boxplot()
+
+# Labels make your axes interpretable
+ggplot(diamonds,aes(x=price)) +
+  geom_histogram() +
+  xlab('Price in US Dollars') +
+  ylab('Count') +
+  ggtitle('Distribution of Diamond Prices')
+
+# OR
+ggplot(diamonds,aes(x=price)) +
+  geom_histogram() +
+  labs(x='Price in US Dollars',
+       y='Count',
+       title='Distribution of Diamond Prices')
+
+
+# And finally, to save a figure as a file
+ggsave('outfile.png', height=3, width=5)
