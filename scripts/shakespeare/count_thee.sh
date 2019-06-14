e358c23011c3e2cdab9926f727e6fd4acd3c3eb4
Matthew Biggs <mbiggs@agbiome.com>
2019-06-13 22:41:27 -0400

    Makefile example updated

--- scripts/shakespeare/count_thee.sh
+++
@@ -1,3 +0,0 @@
-# Count the number of times the word "thee" occurs in Shakespear's works
-
-grep -Eioc "\<thee\>" shakespeare.txt 
--- scripts/shakespeare/dogberry.sh
+++
@@ -1,6 +0,0 @@
-# Extract all the quotes by the character DOGBERRY
-
-grep -Ei -A 10 "[A-Z]{3,}" $1 |
-	tr '\n\r' ' ' | \
-	sed -E 's/([A-Z]{3,})/\n\1/g'  |
-	grep -Ei "^DOGBERRY" > dogberry.txt
