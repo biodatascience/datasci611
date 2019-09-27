91801c6ffa45b251ebe35736bf65ea1dc34289e8
Matt Biggs <mb3ad@virginia.edu>
2019-09-26 22:34:40 -0400

    optimization examples

--- scripts/police_modeling_solution.R
+++ scripts/police_modeling_solution.R
@@ -1,26 +1,27 @@
 library(tidyverse)
 library(reshape2)
+library(modelr)
 
 # Download and format data
-police_df = read_csv(url('https://raw.githubusercontent.com/fivethirtyeight/data/master/police-locals/police-locals.csv'))
+police_df = read_csv(url('https://raw.githubusercontent.com/fivethirtyeight/data/master/police-locals/police-locals.csv'),
+                     na='**')
 
 police_df = police_df %>%
-  melt(id=c('city', 'police_force_size')) %>%
-  mutate(fraction=as.numeric(value))
+  gather(-city, -police_force_size, key='variable', value='value') 
 
 # Plot data
-ggplot(police_df, aes(x=police_force_size, y=fraction, group=variable)) +
+ggplot(police_df, aes(x=police_force_size, y=value, group=variable)) +
   geom_point(aes(color = variable))
 
 # Build model
-mod1 = lm(fraction ~ police_force_size * variable, data=police_df)
+mod1 = lm(value ~ police_force_size * variable, data=police_df)
 
 # Plot model predictions
 predictions = police_df %>%
   data_grid(police_force_size, variable) %>%
   gather_predictions(mod1)
 
-ggplot(police_df, aes(x=police_force_size, y=fraction, group=variable)) +
+ggplot(police_df, aes(x=police_force_size, y=value, group=variable)) +
   geom_point(aes(color = variable)) +
   geom_line(data=predictions, aes(y=pred, color=variable)) +
   facet_wrap(~variable)
